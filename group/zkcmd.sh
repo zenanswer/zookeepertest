befe8107e34462d82eb3e972f25d94ab1cac1d53
zenanswer <zenanswer@hotmail.com>
2018-04-06 21:26:40 +0800

    Add group test cfg file

---
+++ group/zkcmd.sh
@@ -0,0 +1,11 @@
+source setenv.sh
+zkServer.sh $1 zoo_1.cfg
+sleep 2
+zkServer.sh $1 zoo_2.cfg
+sleep 2
+zkServer.sh $1 zoo_3.cfg
+sleep 2
+zkServer.sh $1 zoo_4.cfg
+sleep 2
+zkServer.sh $1 zoo_5.cfg
+sleep 2
