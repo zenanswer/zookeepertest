a1cf9a56c51eb80d9744f1daa605c8ea7d33df6e
zenanswer <zenanswer@hotmail.com>
2018-04-06 08:21:30 +0800

    Add zk cfg for znodetest

---
+++ znodetest/setenv.sh
@@ -0,0 +1,4 @@
+#export ZOOKEEPER_HOME=/home/xcwang/storm/zookeeper-3.4.11/
+export ZOOKEEPER_HOME=/home/xcwang/storm/zookeeper-3.5.3-beta
+export PATH=$ZOOKEEPER_HOME/bin:$PATH
+export ZOOCFGDIR=$(pwd)
